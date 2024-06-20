18a3c0bb1c87d0822d4fba01a15ad874d3874d6c
Sergej Kubat <sergej.kubat18@gmail.com>
2024-06-20 19:41:03 +0200

    Basic scaffolding

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="p-5">
+            <h1 className="font-bold text-3xl text-center">Home</h1>
+        </div>
+    );
+};
+
+export default Home;
